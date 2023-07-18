ef1abab0500b6a76e0c496d9b01812b5eb9e896b
Vanchhai <chhai.chhin51@gmail.com>
2023-07-18 17:36:00 +0700

    first commit

---
+++ App.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WinFormsApp2
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            is_boot = new DataGridViewTextBoxColumn();
+            name = new DataGridViewTextBoxColumn();
+            email = new DataGridViewTextBoxColumn();
+            username = new DataGridViewTextBoxColumn();
+            created_at = new DataGridViewTextBoxColumn();
+            updated_at = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { is_boot, name, email, username, created_at, updated_at });
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(800, 450);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // is_boot
+            // 
+            is_boot.HeaderText = "is_boot";
+            is_boot.Name = "is_boot";
+            // 
+            // name
+            // 
+            name.HeaderText = "name";
+            name.Name = "name";
+            // 
+            // email
+            // 
+            email.HeaderText = "email";
+            email.Name = "email";
+            // 
+            // username
+            // 
+            username.HeaderText = "username";
+            username.Name = "username";
+            // 
+            // created_at
+            // 
+            created_at.HeaderText = "created_at";
+            created_at.Name = "created_at";
+            // 
+            // updated_at
+            // 
+            updated_at.HeaderText = "updated_at";
+            updated_at.Name = "updated_at";
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.System;
+            button1.Location = new Point(12, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Insert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // App
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "App";
+            Text = "App";
+            Load += App_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn email;
+        private DataGridViewTextBoxColumn username;
+        private DataGridViewTextBoxColumn created_at;
+        private DataGridViewTextBoxColumn updated_at;
+        private DataGridViewTextBoxColumn is_boot;
+        private Button button1;
+    }
+}
---
+++ App.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace WinFormsApp2
+{
+
+
+    public partial class App : Form
+    {
+        static HttpClient client = new HttpClient();
+        public string basUrl = "http://127.0.0.1:8000";
+        public int index;
+
+        private byte[] blankImage = new byte[] {
+        0x42, 0x4D, 0xC6, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x76,
+        0x0, 0x0, 0x0, 0x28, 0x0, 0x0, 0x0, 0xB, 0x0, 0x0, 0x0, 0xA,
+        0x0, 0x0, 0x0, 0x1, 0x0, 0x4, 0x0, 0x0, 0x0, 0x0, 0x0, 0x50,
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x10,
+        0x0, 0x0, 0x0, 0x10, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x80, 0x0, 0x0, 0x80, 0x0, 0x0, 0x0, 0x80, 0x80, 0x0,
+        0x80, 0x0, 0x0, 0x0, 0x80, 0x0, 0x80, 0x0, 0x80, 0x80, 0x0,
+        0x0, 0xC0, 0xC0, 0xC0, 0x0, 0x80, 0x80, 0x80, 0x0, 0x0, 0x0,
+        0xFF, 0x0, 0x0, 0xFF, 0x0, 0x0, 0x0, 0xFF, 0xFF, 0x0, 0xFF,
+        0x0, 0x0, 0x0, 0xFF, 0x0, 0xFF, 0x0, 0xFF, 0xFF, 0x0, 0x0,
+        0xFF, 0xFF, 0xFF, 0x0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xF0,
+        0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xF0, 0x0, 0x0,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xF0, 0x0, 0x0, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xF0, 0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xF0, 0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xF0,
+        0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xF0, 0x0, 0x0,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xF0, 0x0, 0x0, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xF0, 0x0, 0x0, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xF0, 0x0, 0x0};
+        public App()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void App_Load(object sender, EventArgs e)
+        {
+            dataGridView1.CellClick += new
+            DataGridViewCellEventHandler(dataGridView1_CellClick);
+            dataGridView1.CellEndEdit += new
+            DataGridViewCellEventHandler(dataGridView1_CellEndEdit);
+
+            dataGridView1.CellFormatting += dataGridView1_CellFormatting;
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[5].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            Bitmap blank = new Bitmap(new MemoryStream(this.blankImage));
+            DataGridViewImageColumn dgvImage = new DataGridViewImageColumn();
+            dgvImage.HeaderText = "Image";
+            dgvImage.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            dgvImage.Image = blank;
+            dataGridView1.Columns.Add(dgvImage);
+            dataGridView1.RowTemplate.Height = 80;
+            await getProductAsync("http://127.0.0.1:8000/api/admin/products/view-all");
+        }
+         async Task<Product> getProductAsync(String url)
+        {
+            Product product = null;
+            HttpResponseMessage response = await client.GetAsync(url);
+            string responseBody = await response.Content.ReadAsStringAsync();
+          //  List<Product> myObjects = JsonConverter.DeserializeObject<List<Product>>(responseBody);
+
+            List<Product> myObjects = JsonConvert.DeserializeObject<List<Product>>(responseBody);
+            foreach (Product obj in myObjects)
+            {
+                Image newImage = Image.FromFile(@"C:\Users\ASUS\source\repos\WinFormsApp2\WinFormsApp2\icon.png");
+                this.dataGridView1.Rows.Add(obj.id, obj.name, obj.sku, obj.name, DateTime.Now, DateTime.Now, newImage);
+
+                Debug.WriteLine($"Name: {obj.name}, Sku: {obj.sku}, Id: {obj.id}");
+            }
+            return product;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           // MessageBox.Show("cell cick");
+        }
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show("CELL END eDIT cick");
+        }
+
+        void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+
+            if (e.ColumnIndex == 0)
+            {
+                if (dataGridView1.Rows[e.RowIndex].Cells[0].Value != null)
+                {
+                    var value = dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+                    if (value.ToString() == "False")
+                    {
+                      
+                      //  dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = blank;
+                        dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.Gray;
+                    }
+                }
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.index += 1;
+            bool isBot = this.index % 2 == 0 ? true : false;
+       
+            Image newImage = Image.FromFile(@"C:\Users\ASUS\source\repos\WinFormsApp2\WinFormsApp2\icon.png");
+            this.dataGridView1.Rows.Add(isBot, "DD", "AAA", "BBDD", DateTime.Now, DateTime.Now, newImage);
+
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check if the changed cell is in the column you want to base the color change on
+            MessageBox.Show("nothing");
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+   
+
+        static void ShowProduct(Product product)
+        {
+            Console.WriteLine($"Name: {product.name}\tPrice: ") ;
+        }
+    }
+
+    public class Product
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public decimal quantity { get; set; }
+        public string sku { get; set; }
+    }
+}
